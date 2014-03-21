5feba52535c1ba525f4853d70f16091de9573c02
Elliott Fouts <efouts@qualtrax.com>
2014-03-21 13:04:32 -0400

    Initial commit

---
+++ TheNewStringCalculator.Tests/SuiteTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TheNewStringCalculator.Tests
+{
+    [TestFixture]
+    public class SuiteTests
+    {
+        [Test]
+        public void AssertTheTruth()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
