b776f85bde9c54fb9bcfea802e339005e6e4a036
Elliott Fouts <efouts@qualtrax.com>
2014-03-21 14:44:10 -0400

    Passing all test except order of operations

---
+++ TheNewStringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TheNewStringCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator(new ExpressionParser());
+        }
+
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("1+1", 2)]
+        [TestCase("2-1", 1)]
+        [TestCase("2*1", 2)]
+        [TestCase("4/2", 2)]
+        [TestCase("2--1", 3)]
+        [TestCase("-5+-3--4", -4)]
+        [TestCase("5*2-4/2+2", 10)]
+        public void AssertCalculation(String input, Int32 expectedResult)
+        {
+            var result = calculator.Calculate(input);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ TheNewStringCalculator/Calculator.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNewStringCalculator
+{
+    public class Calculator
+    {
+        private ExpressionParser parser;
+
+        public Calculator(ExpressionParser parser)
+        {
+            this.parser = parser;
+
+        }
+        public int Calculate(String input)
+        {
+            return parser.Parse(input).Evaluate();
+        }
+    }
+
+    public class ExpressionParser
+    {
+        private String[] operators = new[] { "+", "-", "*", "/" };
+
+        public Expression Parse(String input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return new ConstantExpression("0");
+
+            var @operator = GetFirstOperator(input);
+            var indexOfOperator = input.IndexOf(@operator, 1);
+            
+            if (@operator == String.Empty)
+                return new ConstantExpression(input);
+
+            var left = Parse(input.Substring(0, indexOfOperator));
+            var right = Parse(input.Substring(indexOfOperator + @operator.Length));
+
+            if (@operator == "+")
+                return new AdditionExpression(left, right);
+
+            if (@operator == "-")
+                return new SubtractionExpression(left, right);
+
+            if (@operator == "*")
+                return new MultiplicationExpression(left, right);
+
+            if (@operator == "/")
+                return new DivisionExpression(left, right);
+
+            throw new NotImplementedException();
+        }
+
+        private String GetFirstOperator(String input, Int32 startIndex = 1)
+        {
+            var operatorIndexes = new Dictionary<Int32, String>();
+
+            foreach (var @operator in operators)
+            {
+                var index = input.IndexOf(@operator, startIndex);
+
+                if (index != -1)
+                    operatorIndexes.Add(index, @operator);
+            }
+
+            if (operatorIndexes.Any() == false)
+                return String.Empty;
+
+            var lowestIndex = operatorIndexes.Keys.Min();
+
+            return operatorIndexes[lowestIndex];
+        }
+    }
+
+    public interface Expression
+    {
+        Int32 Evaluate();
+    }
+
+    public class DivisionExpression : Expression
+    {
+        private Expression left, right;
+
+        public DivisionExpression(Expression left, Expression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public Int32 Evaluate()
+        {
+            return left.Evaluate() / right.Evaluate();
+        }
+    }
+
+    public class MultiplicationExpression : Expression
+    {
+        private Expression left, right;
+
+        public MultiplicationExpression(Expression left, Expression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public Int32 Evaluate()
+        {
+            return left.Evaluate() * right.Evaluate();
+        }
+    }
+
+    public class SubtractionExpression : Expression
+    {
+        private Expression left, right;
+
+        public SubtractionExpression(Expression left, Expression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public Int32 Evaluate()
+        {
+            return left.Evaluate() - right.Evaluate();
+        }
+    }
+
+    public class AdditionExpression : Expression
+    {
+        private Expression left, right;
+
+        public AdditionExpression(Expression left, Expression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public Int32 Evaluate()
+        {
+            return left.Evaluate() + right.Evaluate();
+        }
+    }
+
+    public class ConstantExpression : Expression
+    {
+        private String input;
+
+        public ConstantExpression(String input)
+        {
+            this.input = input;
+        }
+    
+        public Int32 Evaluate()
+        {
+ 	        return Convert.ToInt32(input);
+        }
+
+        public override String ToString()
+        {
+            return input;
+        }
+    }
+}
